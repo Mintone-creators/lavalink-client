cfeb8dc86dfba36016d3a803184c159f0e944410
appujet <sdipedit@gmail.com>
2025-07-10 11:14:17 +0530

    feat: add Awaitable type and update QueueStoreManager to use it

--- src/structures/Types/Utils.ts
+++ src/structures/Types/Utils.ts
@@ -170,6 +170,8 @@ export interface Exception {
     message: string;
     /** Cause by lavalink */
     cause: string;
+    /** causeStackTrace by lavalink */
+    causeStackTrace: string;
 }
 
 export interface PlayerEvent {
@@ -611,3 +613,5 @@ export type LavaSearchQuery = {
     /** The Types to filter the search to */
     types?: LavaSearchType[]
 };
+
+export type Awaitable<T> = Promise<T> | T
